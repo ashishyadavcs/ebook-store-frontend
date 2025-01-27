bf34107a7c3a7e0b607ba80ea00b1afd54304851
Ashish Kumar <ashishbhu221306@gmail.com>
2025-01-27 23:31:35 +0530

    cache disabled for homepage ebooks

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import { HomeStyles } from "@/styles/home.styled";
 import Container from "./components/Container";
 import config from "@/config/index.js";
 export default async function Home() {
-    let ebooks=[]
+    let ebooks = [];
     try {
-        const req = await fetch(`${config.APP_URL}/api/ebooks`);
+        const req = await fetch(`${config.APP_URL}/api/ebooks`, {
+            cache: "no-store",
+        });
         const { data } = await req.json();
-        ebooks=[...data]
+        ebooks = [...data];
     } catch (err) {}
     return (
         <HomeStyles>
