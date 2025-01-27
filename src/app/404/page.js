41c84302d3b452e44d333398f425cadfc90d59cb
Ashish Kumar <ashishbhu221306@gmail.com>
2025-01-27 21:59:28 +0530

    usesearcherror fixed by layout whole wrap in suspend

--- src/app/404/page.js
+++ src/app/404/page.js
@@ -1,9 +1,22 @@
+"use client"
+import Container from "@/components/Container";
 import { Suspense } from "react";
+import styled from "styled-components";
 
 export default function Custom404() {
     return (
         <Suspense>
-            <h1>Page Not Found</h1>
+            <Pagestyle>
+                <Container>
+                    <h1>Page Not Found</h1>
+                </Container>
+            </Pagestyle>
         </Suspense>
     );
 }
+const Pagestyle = styled.section`
+min-height: 85vh;
+display: flex;
+align-items: center;
+text-align: center;
+`;
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,17 +5,17 @@ import Config from "@/components/Config";
 import { Suspense } from "react";
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body>
-                <StyledComponentsRegistry>
-                    <Config />
-                    <Header />
-                    <main role="main">
-                        <Suspense fallback={<div>loading...</div>}>{children}</Suspense>
-                    </main>
-                    <Footer />
-                </StyledComponentsRegistry>
-            </body>
-        </html>
+        <Suspense>
+            <html lang="en">
+                <body>
+                    <StyledComponentsRegistry>
+                        <Config />
+                        <Header />
+                        <main role="main">{children}</main>
+                        <Footer />
+                    </StyledComponentsRegistry>
+                </body>
+            </html>
+        </Suspense>
     );
 }
