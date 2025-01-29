1666577bf3f72477bca103a965484da43ddeddf6
Ashish Kumar <ashishbhu221306@gmail.com>
2025-01-29 23:45:28 +0530

    loader added

---
+++ src/app/[slug]/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from '@/components/Container'
+
+const Loading = () => {
+  return (
+    <Container>loading...</Container>
+  )
+}
+
+export default Loading
