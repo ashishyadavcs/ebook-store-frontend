7ce5bcaabd0dd0a1497c9f60fb6c68b349e0e3a7
Ashish Kumar <ashishbhu221306@gmail.com>
2025-02-11 18:03:09 +0530

    ebook details mobile thumbnail

--- src/app/components/ebook/Ebookdetail.jsx
+++ src/app/components/ebook/Ebookdetail.jsx
@@ -3,16 +3,29 @@ import Container from "@/components/ui/Container";
 import Review from "@/components/Review";
 import Image from "next/image";
 import Ebookdetails from "@/styles/ebookdetails.styled";
-import config from "@/config/index";
 import AddTocart from "@/components/ebook/AddTocart";
+import { useServerSideFetch } from "@/utils/ssr-api-call";
 const Ebookdetail = async ({ id }) => {
-    const req = await fetch(`${config.APP_URL}/api/ebooks/${id}`, {
-        cache: "no-store",
-    });
-    let { data: ebook } = await req.json();
-    ebook = ebook[0];
+    let user, ebook;
+    try {
+        // const user = await useServerSideFetch("/api/user");
+        // console.log(user);
+        const ebookResult = await useServerSideFetch(`/api/ebooks/${id}`);
+        ebook = ebookResult?.data[0];
+    } catch (err) {
+        console.log(err);
+    }
+
     return (
         <Ebookdetails>
+            <span className="thumbnail mobile">
+                <Image
+                    priority
+                    layout="fill"
+                    alt="ebook"
+                    src={ebook?.coverImageUrl || "/images/ebook.jpg"}
+                />
+            </span>
             <Container>
                 <span className="thumbnail">
                     <Image
