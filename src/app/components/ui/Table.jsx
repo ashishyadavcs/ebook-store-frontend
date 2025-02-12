d783f8d56c390186db2c148035a20a54d0f15d72
Ashish Kumar <ashishbhu221306@gmail.com>
2025-02-12 22:41:59 +0530

    admin ebooks data fix

--- src/app/components/ui/Table.jsx
+++ src/app/components/ui/Table.jsx
@@ -4,7 +4,7 @@ import { FaEdit } from "react-icons/fa";
 import Button from "./Button";
 import Link from "next/link";
 import DeleteEbook from "../ebook/DeleteEbook";
-const Table = ({ data }) => {
+const Table = ({ data = [] }) => {
     return (
         <TableStyle>
             <table>
