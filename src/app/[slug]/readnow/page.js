2d856ae6420e60fae4619570b71559da4c254c2c
Ashish Kumar <ashishbhu221306@gmail.com>
2025-03-15 11:48:18 +0530

    readnow page text

--- src/app/[slug]/readnow/page.js
+++ src/app/[slug]/readnow/page.js
@@ -4,28 +4,6 @@ const Page = async () => {
     return (
         <Readingstyle className="container">
             <h1>design in progress</h1>
-            <article>
-                <p>
-                    Ray is a variation of the title "Rai" that is used by some Yadavs. "Rai" is a
-                    title that is primarily used for men, while "Rani" is used for women. Other
-                    variations of "Rai" include "Roy" and "Rao".{" "}
-                </p>
-                <p>
-                    Ray is a variation of the title "Rai" that is used by some Yadavs. "Rai" is a
-                    title that is primarily used for men, while "Rani" is used for women. Other
-                    variations of "Rai" include "Roy" and "Rao".{" "}
-                </p>
-                <p>
-                    Ray is a variation of the title "Rai" that is used by some Yadavs. "Rai" is a
-                    title that is primarily used for men, while "Rani" is used for women. Other
-                    variations of "Rai" include "Roy" and "Rao".{" "}
-                </p>
-                <p>
-                    Ray is a variation of the title "Rai" that is used by some Yadavs. "Rai" is a
-                    title that is primarily used for men, while "Rani" is used for women. Other
-                    variations of "Rai" include "Roy" and "Rao".{" "}
-                </p>
-            </article>
         </Readingstyle>
     );
 };
