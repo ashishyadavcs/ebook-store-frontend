4529a9ce1bf104143b99f1d59e357ff68fdfccaf
Ashish Kumar <ashishbhu221306@gmail.com>
2025-04-26 20:24:58 +0530

    window reload removed

--- src/app/components/Googlelogin.jsx
+++ src/app/components/Googlelogin.jsx
@@ -29,7 +29,6 @@ const Googlelogin = ({ title = "Login with" }) => {
 
         if (result.success) {
             router.refresh();
-            window.location.reload();
             const isAdmin = result.user.role == "admin";
             router.push(searchParams.get("from") || (isAdmin ? "/admin" : "/dashboard"));
             toastify.info("Refresh page if not redirecting automatically");
