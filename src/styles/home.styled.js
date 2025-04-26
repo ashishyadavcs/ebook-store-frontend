6e477e36a8161af875dc807182889687e38b8e9a
Ashish Kumar <ashishbhu221306@gmail.com>
2025-04-26 14:04:54 +0530

    home title shadow

--- src/styles/home.styled.js
+++ src/styles/home.styled.js
@@ -7,6 +7,7 @@ export const HomeStyles = styled.section`
         margin: 40px auto;
         font-size: clamp(2rem, 8vw, 4rem);
         line-height: 1.3;
+        filter: drop-shadow(2px 1px 1px black);
         ${media.sm} {
             margin: 20px 30px 20px;
         }
