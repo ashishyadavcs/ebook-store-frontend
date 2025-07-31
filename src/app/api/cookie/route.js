d32fdcd2814dc22102e6ad0b528ed4566067546c
Ashish Kumar <ashishbhu221306@gmail.com>
2025-08-01 01:47:04 +0530

    get and set cookie api route

--- src/app/api/cookie/route.js
+++ src/app/api/cookie/route.js
@@ -5,10 +5,34 @@ export async function POST(req) {
     try {
         const { name, value, options = {} } = await req.json();
         const cookie = await cookies();
-
+        cookie.set(name, value, {
+            ...options,
+        });
         return NextResponse.json(
             {
                 success: true,
+            },
+            {
+                status: 200,
+            }
+        );
+    } catch (err) {
+        return NextResponse.json(
+            { success: false },
+            {
+                status: 500,
+            }
+        );
+    }
+}
+export async function GET(req) {
+    try {
+        const cookie = await cookies();
+        const allCookies = cookie.getAll();
+        return NextResponse.json(
+            {
+                success: true,
+                cookies: allCookies,
             },
             {
                 status: 200,
